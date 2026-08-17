ba131c827b0d3913492083af98a7fcee53995f40
ShiroGeo <inocgeorgealfeser@gmail.com>
2026-08-18 07:53:58 +0800

    Changes the is feature to boolean true and false than number 1

--- Frontend/src/pages/Homepage.vue
+++ Frontend/src/pages/Homepage.vue
@@ -88,7 +88,7 @@ const roles = ref([
 
 const fetchVenues = async () => {
     const response = await useVenue.getList();
-    featuredFacility.value = response.find(ar => ar.venue.is_featured === 1);
+    featuredFacility.value = response.find(ar => ar.venue.is_featured === true);
     const lowestStartingPrice = response.flatMap(venueAdmin =>
         venueAdmin.venue?.courts?.map(court => Number(court.price)) ?? []
     )
